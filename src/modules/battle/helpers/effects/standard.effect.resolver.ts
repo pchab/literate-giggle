27654f58ad28171f83b6fa37d4e58425097f6ee1
Pierre Chabardes <pierre@chabardes.net>
2026-03-26 14:49:58 +0100

    fix for projectile animation

--- src/modules/battle/helpers/effects/standard.effect.resolver.ts
+++ src/modules/battle/helpers/effects/standard.effect.resolver.ts
@@ -15,84 +15,95 @@ import { applyEffectToEntity, resolveTargets } from "./effect.helpers";
 import type { EffectResolverParams } from "./effect.resolvers";
 
 const animateProjectile =
-	(get: StoreGet, set: StoreSet) =>
-	async (
-		projectile: VfxType,
-		originPos: GridPosition,
-		targetPos: GridPosition,
-	) => {
-		const { currentVfx } = get();
-		const projectileId = crypto.randomUUID();
+	(_: StoreGet, set: StoreSet) =>
+		async (
+			projectile: VfxType,
+			originPos: GridPosition,
+			targetPos: GridPosition,
+		) => {
+			const projectileId = crypto.randomUUID();
 
-		const dx = targetPos.col - originPos.col;
-		const dy = targetPos.row - originPos.row;
-		const angle = Math.atan2(dx, dy) * (180 / Math.PI);
+			const dx = targetPos.col - originPos.col;
+			const dy = targetPos.row - originPos.row;
+			const angle = Math.atan2(dx, dy) * (180 / Math.PI);
 
-		set(() => ({
-			currentVfx: {
-				...currentVfx,
-				[getCellId(originPos)]: {
+			set((prev) => ({
+				currentVfx: {
+					...prev.currentVfx,
+					[getCellId(originPos)]: {
+						type: projectile,
+						id: projectileId,
+						angle,
+					},
+				},
+			}));
+
+			await sleep(100);
+
+			set((prev) => {
+				const nextVfx = { ...prev.currentVfx };
+				delete nextVfx[getCellId(originPos)];
+
+				nextVfx[getCellId(targetPos)] = {
 					type: projectile,
 					id: projectileId,
 					angle,
-				},
-			},
-		}));
-		await sleep(100);
-		set(() => ({
-			currentVfx: {
-				...currentVfx,
-				[getCellId(targetPos)]: {
-					type: projectile,
-					id: projectileId,
-					angle,
-				},
-			},
-		}));
-		await sleep(300);
-	};
+				};
+				return { currentVfx: nextVfx };
+			});
+
+			await sleep(300);
+
+			set((prev) => {
+				const nextVfx = { ...prev.currentVfx };
+				if (nextVfx[getCellId(targetPos)]?.id === projectileId) {
+					delete nextVfx[getCellId(targetPos)];
+				}
+				return { currentVfx: nextVfx };
+			});
+		};
 
 export const resolveStandardEffect =
 	(get: StoreGet, set: StoreSet, isSimulation = false) =>
-	(effect: DamageEffect | HealEffect | ApplyStatusEffect) =>
-	async <C extends BattleUnit>({
-		anchorTarget,
-		caster,
-		patternCells,
-	}: EffectResolverParams<C>): Promise<void> => {
-		const { heroes, monsters, summons } = get();
-		const figures = [...heroes, ...monsters, ...summons];
-		const targets = resolveTargets<BattleUnit>(
-			effect.target,
-			anchorTarget,
-			caster,
-			figures,
-			patternCells,
-		);
-		const targetPositions: GridPosition[] = [];
-		if (anchorTarget && effect.projectile && !isSimulation) {
-			await animateProjectile(get, set)(
-				effect.projectile,
-				caster.gridPosition,
-				anchorTarget.gridPosition,
-			);
-		}
+		(effect: DamageEffect | HealEffect | ApplyStatusEffect) =>
+			async <C extends BattleUnit>({
+				anchorTarget,
+				caster,
+				patternCells,
+			}: EffectResolverParams<C>): Promise<void> => {
+				const { heroes, monsters, summons } = get();
+				const figures = [...heroes, ...monsters, ...summons];
+				const targets = resolveTargets<BattleUnit>(
+					effect.target,
+					anchorTarget,
+					caster,
+					figures,
+					patternCells,
+				);
+				const targetPositions: GridPosition[] = [];
+				if (anchorTarget && effect.projectile && !isSimulation) {
+					await animateProjectile(get, set)(
+						effect.projectile,
+						caster.gridPosition,
+						anchorTarget.gridPosition,
+					);
+				}
 
-		for (const targetId of targets) {
-			const target = findUnit(get)(targetId);
-			if (!target) continue;
+				for (const targetId of targets) {
+					const target = findUnit(get)(targetId);
+					if (!target) continue;
 
-			const updatedEntity = applyEffectToEntity({ entity: target, effect });
-			targetPositions.push(updatedEntity.gridPosition);
-			await updateBattleUnitState(get, set, isSimulation)(updatedEntity);
-		}
+					const updatedEntity = applyEffectToEntity({ entity: target, effect });
+					targetPositions.push(updatedEntity.gridPosition);
+					await updateBattleUnitState(get, set, isSimulation)(updatedEntity);
+				}
 
-		set((prev) => {
-			const newVfx = getVfxForEffect(effect, targetPositions);
+				set((prev) => {
+					const newVfx = getVfxForEffect(effect, targetPositions);
 
-			return {
-				...prev,
-				currentVfx: { ...prev.currentVfx, ...newVfx },
+					return {
+						...prev,
+						currentVfx: { ...prev.currentVfx, ...newVfx },
+					};
+				});
 			};
-		});
-	};
